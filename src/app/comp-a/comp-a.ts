924ecdfcd601ac650518f14f9a50be87437e96a5
Thomas Többe <toebbe.thomas@outlook.de>
2026-04-21 10:38:54 +0200

    feat: Router and Routres

---
+++ src/app/comp-a/comp-a.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-a',
+  imports: [],
+  templateUrl: './comp-a.html',
+  styleUrl: './comp-a.scss',
+})
+export class CompA {}
---
+++ src/app/comp-b/comp-b.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-b',
+  imports: [],
+  templateUrl: './comp-b.html',
+  styleUrl: './comp-b.scss',
+})
+export class CompB {}
