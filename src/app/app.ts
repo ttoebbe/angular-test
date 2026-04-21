e8cd8c88080e9fe886703972f22d9f8ab0a72207
Thomas Többe <toebbe.thomas@outlook.de>
2026-04-21 11:12:29 +0200

    feat: routerlink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink,RouterOutlet } from '@angular/router';
 import { Header } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
