d7901ea46a66adf4ad58bb0c909c40eda052f15d
Thomas Többe <toebbe.thomas@outlook.de>
2026-04-21 11:04:14 +0200

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CompA } from './comp-a/comp-a';
+import { CompB } from './comp-b/comp-b';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:"",
+        component: CompA
+    },
+    {
+        path: "compb",
+        component: CompB
+    }
+
+];
