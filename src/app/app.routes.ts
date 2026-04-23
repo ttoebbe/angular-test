bf9bbd2bbbaed4591c37b62c427bdb41369345d3
Thomas Toebbe <toebbe.thomas@outlook.de>
2026-04-23 14:27:19 +0200

    feat: 404 not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,27 @@ import { Routes } from '@angular/router';
 import { CompA } from './comp-a/comp-a';
 import { CompB } from './comp-b/comp-b';
 import { WrapperHome } from './wrapper-home/wrapper-home';
-
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
-    {
-        path:"",
-        component: CompA
-    },
-    {
-        path: "home",
-        component: WrapperHome
-    },
-    {
-        path: "compb/:myParam",
-        component: CompB
-    }, 
-    {
-        path: "compc",
-        redirectTo: ""
-    },
-    {
-        path: "**",
-        redirectTo: ""
-    }
-
+  {
+    path: '',
+    component: CompA,
+  },
+  {
+    path: 'home',
+    component: WrapperHome,
+  },
+  {
+    path: 'compb/:myParam',
+    component: CompB,
+  },
+  {
+    path: 'compc',
+    redirectTo: '',
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
 ];
